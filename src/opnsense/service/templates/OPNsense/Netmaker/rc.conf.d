75204fcbc143c3093658a36400103ddf827eeac4
Kevin Scott Adams <kevin.adams@ksatechnologies.com>
2023-05-27 11:39:23 -0400

    Added mosquitto.
    - Added the values for mosquitto to be started as a service when
    Netmaker is enabled.

--- src/opnsense/service/templates/OPNsense/Netmaker/rc.conf.d
+++ src/opnsense/service/templates/OPNsense/Netmaker/rc.conf.d
@@ -1,6 +1,8 @@
 {# Macro import #}
 {% if not helpers.empty('OPNsense.Netmaker.general.enabled') %}
 netmaker_enable="YES"
+mosquitto_enable="YES"
 {% else %}
 netmaker_enable="NO"
+mosquitto_enable="NO"
 {% endif %}
